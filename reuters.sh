eee52340d33de363ead2d9f99b0194846226c3ee
glmck13 <glmck13@verizon.net>
2017-07-03 16:10:25 -0400

    Add files via upload

--- reuters.sh
+++ reuters.sh
@@ -1,3 +1,4 @@
 #!/bin/ksh
 
-curl -s http://feeds.reuters.com/reuters/topNews | grep '.<description>' | sed -e "s/<description>//g" -e "s/\&[gl]t.*//"
+curl -s http://feeds.reuters.com/reuters/topNews | grep '.<description>' |
+	sed -e "s/<description>//g" -e "s/\&[gl]t.*//" -e "s/\&amp;/\&/g"
