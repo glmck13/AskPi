b40d3664e3bd529d4021cdaa627cccf1d773752b
glmck13 <glmck13@verizon.net>
2017-07-03 16:11:52 -0400

    Add files via upload

--- apnews.sh
+++ apnews.sh
@@ -1,3 +1,4 @@
 #!/bin/ksh
 
-curl -s 'http://hosted.ap.org/dynamic/fronts/HOME?SITE=AP&SECTION=HOME' | grep apHeadline | sed -e "s/<[^>]*>/. /g" -e "s/\&quot;/\"/g" -e "s/\&apos;/\'/g" -e "s/ \.//g" -e "s/^. //"
+curl -s 'http://hosted.ap.org/dynamic/fronts/HOME?SITE=AP&SECTION=HOME' | grep apHeadline |
+	sed -e "s/<[^>]*>/. /g" -e "s/\&quot;/\"/g" -e "s/\&apos;/\'/g" -e "s/ \.//g" -e "s/^. //"
