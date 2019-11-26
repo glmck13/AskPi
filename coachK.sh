080a9f002f2eb6f0ba92f04bc6438682a14c167d
glmck13 <glmck13@verizon.net>
2019-11-26 13:54:27 -0500

    Update coachK.sh

--- coachK.sh
+++ coachK.sh
@@ -6,9 +6,9 @@ Request=$1
 case $Request in
 
 game)
-	tail=80
+	comptail=200 hidetail=80
 	print "<p>Here are details about our next game: \\c"
-	echo -n $(curl -s https://goduke.com/sports/mens-basketball/schedule | grep -A $tail Hide | tail -$tail | egrep "<span|<img" | sed -e "s/.*alt=.//" -e "s/ Logo.*//" -e "s/<[^>]*>//g" -e "s/TV://" -e "s/ vs//" | tr -c "[:print:]" " ")
+	echo -n $(curl -s https://goduke.com/sports/mens-basketball/schedule | grep -A$comptail sidearm-schedule-game-completed | grep -A$hidetail Hide | tail -$hidetail | egrep "<span|<img" | sed -e "s/.*alt=.//" -e "s/ Logo.*//" -e "s/<[^>]*>//g" -e "s/TV://" -e "s/ vs//" | tr -c "[:print:]" " ")
 	print "</p>"
 	;;
 
