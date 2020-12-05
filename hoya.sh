f85d992247358b7bef0c8780ec816ec943e51f38
glmck13 <glmck13@verizon.net>
2020-12-05 14:20:16 -0500

    Add files via upload

--- hoya.sh
+++ hoya.sh
@@ -8,24 +8,23 @@ case $Request in
 game)
 	comptail=200 hidetail=75
 	print "<p>Here are details about our next game: \\c"
-	echo -n $(curl -s http://www.guhoyas.com/sports/mens-basketball/schedule | grep -A$comptail sidearm-schedule-game-completed | grep -A$hidetail Hide | tail -$hidetail | recode -f html..ascii | sed -e "s/<[^>]*>//g" -e "s/*//g" -e "s/ vs/versus/" -e "s/Live//" -e "s/Stats//" -e "s/Tickets//" -e "s/Preview//" | tr -c "[:print:]" " ")
+	echo -n $(curl -s "https://guhoyas.com/sports/mens-basketball/schedule" | grep -A$comptail sidearm-schedule-game-completed | grep -A$hidetail Hide | tail -$hidetail | recode -f html..ascii | sed -e "s/<[^>]*>//g" -e "s/*//g" -e "s/ vs/versus/" -e "s/Live//" -e "s/Stats//" -e "s/Tickets//" -e "s/Preview//" | tr -c "[:print:]" " ")
 	print "</p>"
 	;;
 
 recap|notes)
 	outfile="/tmp/$Request$$.html"
-	url=$(curl -s http://www.guhoyas.com/sports/mens-basketball/schedule | grep -i recap | tail -1 | sed -e 's/.*href="\([^"]*\)".*/\1/')
-	curl -s http://www.guhoyas.com/$url >$outfile
+	url=$(curl -s "https://guhoyas.com/sports/mens-basketball/schedule" | grep -i recap | tail -1 | sed -e 's/.*href="\([^"]*\)".*/\1/')
+	url+="?print=true"
+	curl -s "https://guhoyas.com/$url" >$outfile
 	ed "$outfile" <<-"EOF" >/dev/null 2>&1
-		/<strong>/-1
-		1,.d
 		$
 		?<strong>?+2
 		.,$d
 		w
 		q
 	EOF
-	print "<p>$(recode -f html..ascii <$outfile | sed -e "s/<[^>]*>//g" -e "s-//-,-g" -e "s/\[//g" -e "s/\]//g" | tr -c "[:print:]" " " | sed -e "s/ \+/ /g" -e "s/\([A-Z][A-Z ]\+ \)/- \1 - /g" | cut -c1-7990)</p>"
+	print "<p>$(recode -f html..ascii <$outfile | sed -e "s/<[^>]*>/ /g" -e "s-//-,-g" -e "s/\[//g" -e "s/\]//g" | tr -c "[:print:]" " " | sed -e "s/ \+/ /g" -e "s/\([A-Z][A-Z ]\+ \)/- \1 - /g" | cut -c1-7990)</p>"
 	rm -f "$outfile"
 	;;
 
