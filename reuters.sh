22c5453c2cffb15f67dfc02c50575d913593c49d
glmck13 <glmck13@verizon.net>
2023-06-15 15:18:50 -0400

    Delete reuters.sh

--- reuters.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-
-curl -s http://feeds.reuters.com/reuters/topNews |
-	grep -E '.<title>|.<description>' |
-	sed -e "s/<[^>]*>/ - /g" -e "s/\&[gl]t.*//" -e "s/\&amp;/\&/g" -e "s/\([A-Z]\)\./\1/g" | tr -c "[:print:]" " " | cut -c1-7990
