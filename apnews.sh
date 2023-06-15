b336dcddcc65008a45cb33563dbb664bb64be1ae
glmck13 <glmck13@verizon.net>
2023-06-15 15:18:20 -0400

    Delete apnews.sh

--- apnews.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-
-curl -s 'http://hosted.ap.org/dynamic/fronts/HOME?SITE=AP&SECTION=HOME' | grep apHeadline |
-	sed -e "s/<[^>]*>/. /g" -e "s/\&quot;/\"/g" -e "s/\&apos;/\'/g" -e "s/\([A-Z]\)\./\1/g" \
-	-e "s/\. \+/./g" -e "s/\.\+/./g" -e "s/^\.//"
